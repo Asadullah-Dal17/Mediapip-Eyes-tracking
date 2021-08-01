5be01db705f31fe7e93f67cd26b3c157eb0d785a
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-08-01 12:40:12 +0500

    Complete getting Eye Ratio of both Eyes

--- eyes_tracking/main.py
+++ eyes_tracking/main.py
@@ -27,31 +27,33 @@ def landmarks_detector(image, results, draw=False):
     # return the image and point of landmarks all 
     return image, mesh_points_list
 
-def blink_ratio(frame,landmarks, eye_points):
+def blink_ratio(frame,landmarks, eye_points, right_eye=False):
     # horizontal line data 
     hx, hy = landmarks[eye_points[0]][1]
-    
     hx1, hy1 = landmarks[eye_points[7]][1]
     # Top_Vertical Points 
     second_x,vy11 = landmarks[eye_points[12]][1]
     first_x,vy = landmarks[eye_points[11]][1]
-    padding = int((first_x -second_x)/2)
-    # bottom Vertical Point 
     vx1, vy1 = landmarks[eye_points[4]][1]
-    # print(padding)
-    # cv.circle(frame, (first_x+padding, vy ), 4,(0,255,255), -1) 
-    # cv.circle(frame, (hx,hy ), 2,(0,255,255), -1)
-    # cv.circle(frame, (hx1,hy1 ), 2,(0,255,255), -1)
-    # cv.circle(frame, (second_x,vy11 ), 2,(0,255,255), -1)
-    cv.line(frame, (hx, hy), (hx1, hy1), (255,0,0),2)
-    cv.line(frame, (vx1, vy1), (second_x+padding,vy ), (0,255,255))
+    
+    if right_eye:
+        second_x,vy11 = landmarks[eye_points[13]][1]
+        first_x,vy = landmarks[eye_points[12]][1]
+        cv.circle(frame, ( landmarks[eye_points[13]][1] ), 2,(0,255,0), -1)
+        cv.circle(frame, ( landmarks[eye_points[12]][1] ), 2,(0,0,255), -1) 
+        padding = int((first_x- second_x)/2)
+        cv.line(frame, (vx1, vy1), (second_x+padding,vy ), (0,0,255),2)
+    else:
+        padding = int((first_x -second_x)/2)
+        cv.line(frame, (vx1, vy1), (second_x+padding,vy ), (0,255,255))
+
+        print("left")
+   
+    cv.line(frame, (hx, hy), (hx1, hy1), (0,255,0),2)
     eye_pixel_with = hx1-hx 
     # print(h_px)
     eye_pixel_height = vy1 -vy
     ratio = eye_pixel_with/ eye_pixel_height
-    # print(ratio)
-
-
 
     return ratio
     # print('x , y ', x,' , ',y , 'x1 , y1 ', x1, ' , ',y1)
@@ -74,13 +76,11 @@ with mp_face_mesh.FaceMesh(
         results = face_mesh.process(rgb_frame)
         if results.multi_face_landmarks:
             image, points = landmarks_detector(rgb_frame, results)
-            # print(len(LEFT_EYE))
+            [cv.circle(frame, land[1],2, (0,250,50),-1) for land in points]
             
+            #Drwing All Eyes Points 
             # [cv.circle(frame, (points[pos][1]), 1,(0,0,255), -1) for pos in LEFT_EYE]
             # [cv.circle(frame, (points[pos][1]), 1,(255,0,255), -1) for pos in RIGHT_EYE]
-            
-            # right_h=points[RIGHT_EYE[0]][1]
-            # right_h2=points[RIGHT_EYE[7]][1]
             
             top_point =points[LEFT_EYE[4]][1]
             bottom_point=points[LEFT_EYE[11]][1]
@@ -88,17 +88,16 @@ with mp_face_mesh.FaceMesh(
             first_x, first_y = points[LEFT_EYE[11]][1]
             # cv.circle(frame, (points[LEFT_EYE[11]][1]), 2,(255,255,0), -1)
             # cv.circle(frame, (points[LEFT_EYE[12]][1]), 2,(0,255,0), -1)
-            ratio=blink_ratio(frame ,points, LEFT_EYE)
+            ratio=blink_ratio(frame ,points, RIGHT_EYE, right_eye=True)
+            # ratio=blink_ratio(frame ,points, LEFT_EYE)
             second_x = points[LEFT_EYE[12]][1][0]
             pad_to_center = int((first_x -second_x)/2)
             cv.circle(frame, (second_x+pad_to_center,first_y ), 1,(255,0,255), -1) 
             # print(" te ",pad_to_center)
             lf_x = points[LEFT_EYE[0]][1]
             lf_x1 = points[LEFT_EYE[7]][1][0]
-
             y = top_point[1]
             y1 = bottom_point[1]
-
             dif =(lf_x1-lf_x[0])/(y-y1)
             if ratio>5:
                 print("blink")
