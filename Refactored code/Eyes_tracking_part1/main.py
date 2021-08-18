fd9375222a910ed2be593dced1e4a16eb3102f4b
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-08-18 23:31:04 +0500

    updating

--- Refactored code/Eyes_tracking_part1/main.py
+++ Refactored code/Eyes_tracking_part1/main.py
@@ -15,8 +15,9 @@ import time
 FACE_OVAL=[ 10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103,67, 109]
 
 # lips indices for Landmarks
-LIPS=[ 61, 146, 91, 181, 181, 84, 17, 314, 405, 321, 375, 61, 185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 78, 95 ,88 ,178 ,87, 14, 317, 402, 318, 324, 78, 191, 80, 81, 82, 13, 312, 311, 310, 415 ]
-
+LIPS=[ 61, 146, 91, 181, 84, 17, 314, 405, 321, 375,291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95,185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78 ]
+LOWER_LIPS =[61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
+UPPER_LIPS=[ 185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78] 
 # Left eyes indices 
 LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
 LEFT_EYEBROW =[ 336, 296, 334, 293, 300, 276, 283, 282, 295, 285 ]
@@ -28,6 +29,7 @@ RIGHT_EYEBROW=[ 70, 63, 105, 66, 107, 55, 65, 52, 53, 46 ]
 # Variables 
 # Frame per seconds
 frame_counter =0
+fonts = cv.FONT_HERSHEY_COMPLEX
 
 # Setting up mediapipe 
 map_face_mesh= mp.solutions.face_mesh
@@ -47,7 +49,7 @@ def faceLandmarksDetector(img, result, draw=False):
     return mesh_cord_point
 
 # setting up camera 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture('Testing_video.mp4')
 
 # configring mediapipe for face mesh detection
 with map_face_mesh.FaceMesh( min_detection_confidence=0.5, min_tracking_confidence=0.5 ) as face_mesh:
@@ -57,6 +59,7 @@ with map_face_mesh.FaceMesh( min_detection_confidence=0.5, min_tracking_confiden
     while True:
         ret, frame = cap.read()
         frame_counter +=1
+        frame = cv.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv.INTER_CUBIC)
         
         # converting color space from BGR to RGB 
         rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
@@ -73,32 +76,44 @@ with map_face_mesh.FaceMesh( min_detection_confidence=0.5, min_tracking_confiden
             mesh_cords =faceLandmarksDetector(img=frame, result=results)
 
             
-            frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in FACE_OVAL],color=utils.ORANGE, opacity=0.4)
+            frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in FACE_OVAL],color=utils.BLACK, opacity=0.45)
             # draw lips landmarks portion 
             frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in LIPS],color=utils.WHITE, opacity=0.6)
+            # frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in LOWER_LIPS],color=utils.RED, opacity=0.3)
+            # frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in UPPER_LIPS],color=utils.YELLOW, opacity=0.3)
+
             frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in LEFT_EYE],color=utils.YELLOW, opacity=0.4)
             frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in LEFT_EYEBROW],color=utils.GREEN, opacity=0.4)
             frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in RIGHT_EYE],color=utils.YELLOW, opacity=0.4)
             frame = utils.fillPolyTrans(img=frame,points=[mesh_cords[p] for p in RIGHT_EYEBROW],color=utils.GREEN, opacity=0.4)
-            
-            [cv.circle(frame,mesh_cords[p], 2, utils.GREEN, -1 ) for p in FACE_OVAL]
-            
-            [cv.circle(frame,mesh_cords[p], 2, utils.BLACK, -1 ) for p in LIPS]
+            crThickness =1
+            [cv.circle(frame,mesh_cords[p], crThickness, utils.GREEN, 1 ) for p in FACE_OVAL]
 
-            [cv.circle(frame,mesh_cords[p], 2, utils.PINK, -1 ) for p in LEFT_EYE]
-            [cv.circle(frame,mesh_cords[p], 2, utils.BLACK, -1 ) for p in LEFT_EYEBROW]
+            [cv.circle(frame,mesh_cords[p], crThickness, utils.BLACK, 1 ) for p in LIPS]
+
+            [cv.circle(frame,mesh_cords[p], crThickness, utils.PINK, 1 ) for p in LEFT_EYE]
+            [cv.circle(frame,mesh_cords[p], crThickness, utils.ORANGE, 1 ) for p in LEFT_EYEBROW]
             
-            [cv.circle(frame,mesh_cords[p], 2, utils.PINK, -1 ) for p in RIGHT_EYE]
-            [cv.circle(frame,mesh_cords[p], 2, utils.BLACK, -1 ) for p in RIGHT_EYEBROW]
+            [cv.circle(frame,mesh_cords[p], crThickness, utils.PINK, 1 ) for p in RIGHT_EYE]
+            [cv.circle(frame,mesh_cords[p], crThickness, utils.ORANGE, 1 ) for p in RIGHT_EYEBROW]
+            # [cv.putText(frame, f'{p}', mesh_cords[p], fonts, 1.2, (0,255,0),2 ) for p in range(0, 468)]
+            # [cv.circle(frame,mesh_cords[p], crThickness, utils.GREEN, 3 ) for p in range(0, 468)]
+            
+            # [print(p, " ", mesh_cords[p])   for p in range(0, 468)]
+
+
+            # cv.putText(frame, f'', (40,55), font, 1.2, (0,255,0),2 )  
         # calculating the end time of total frames 
         end_time = time.time()- starting_time
 
         # calculating the frames per seconds 
         fps = frame_counter/end_time
         
-        
+        # cv.imwrite( f'images/frame{frame_counter}.png', frame)
         cv.imshow('frame',frame)
-        
+        # if frame_counter ==2:
+        #     break
+        print(frame_counter)
         key = cv.waitKey(1)
         if key==ord('q'):
             break
